d83d5aaad90e5cda60206c09cbbdd2939f6d326a
Kevin <kevinlamdata@gmail.com>
2023-03-09 08:18:38 -0500

    Fixed webscraper. New attempt with implicity wait try/except clause

--- webscraping/games.py
+++ webscraping/games.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 import re
+from datetime import timedelta
 import time
 import numpy as np
 import pandas as pd
@@ -66,22 +67,32 @@ def update_games():
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
         # Set implicit wait time
-        driver.implicitly_wait(5)
+        # driver.implicitly_wait(5)
 
         # Wait to load the page.
         time.sleep(1)
 
-        # Calculate new scroll height and compare with last scroll height.
-        new_height = driver.execute_script("return document.body.scrollHeight")
+        # try/catch block to handle TimeoutException in EC2
+        try:
+            # Calculate new scroll height and compare with last scroll height.
+            new_height = driver.execute_script("return document.body.scrollHeight")
+        except TimeoutException:
+            driver.implicitly_wait(10)
+            new_height = driver.execute_script("return document.body.scrollHeight")
+
+        # reset implicit wait
+        driver.implicitly_wait(0)
 
         if new_height == last_height:
             break
         last_height = new_height
     i = 0
+    startTime = time.time()
     totalFinds = len(element.find_elements(By.CSS_SELECTOR, 'a'))
     for row in element.find_elements(By.CSS_SELECTOR, 'a'):
         inside_array = []
-        print("Progress: "+"{0:.2%}".format(i/totalFinds))
+        endTime = time.time()
+        print(f"Progress: "+"{0:.2%}. Time elapsed: {1}".format(i/totalFinds, timedelta(seconds=endTime-startTime)))
         # print("scrape row " + str(i/totalFinds))
         i = i + 1
         unique_id = np.int64(row.get_attribute('data-ds-appid'))
@@ -94,60 +105,60 @@ def update_games():
             inside_array.append(row.get_attribute('href'))
             inside_array.append(row.find_element(By.CLASS_NAME, 'title').get_attribute('innerHTML').replace(u"\u2122", '').replace(u"\u00ae", '').replace('&amp;','&'))
 
-        temp = row.get_attribute('data-ds-tagids')
-        if temp is not None:
-            new_tags = json.loads(temp)
-            list_genre = []
-            for tag in new_tags:
-                list_genre.append(tag_dict['genre'][int(tag)])
-            inside_array.append(list_genre)
-        else:
-            inside_array.append('None Listed')
+            temp = row.get_attribute('data-ds-tagids')
+            if temp is not None:
+                new_tags = json.loads(temp)
+                list_genre = []
+                for tag in new_tags:
+                    list_genre.append(tag_dict['genre'][int(tag)])
+                inside_array.append(list_genre)
+            else:
+                inside_array.append('None Listed')
 
-        if len(row.find_elements(By.CLASS_NAME, 'win')) > 0:
-            inside_array.append(1)
-        else:
-            inside_array.append(0)
-        if len(row.find_elements(By.CLASS_NAME, 'mac')) > 0:
-            inside_array.append(1)
-        else:
-            inside_array.append(0)
-        if len(row.find_elements(By.CLASS_NAME, 'linux')) > 0:
-            inside_array.append(1)
-        else:
-            inside_array.append(0)
-        
-        if len(row.find_elements(By.CLASS_NAME, 'positive')) > 0 or len(row.find_elements(By.CLASS_NAME, 'mixed')) > 0 or len(row.find_elements(By.CLASS_NAME, 'negative')) > 0:
-            review = row.find_element(By.CLASS_NAME, 'search_review_summary').get_attribute('data-tooltip-html')
-            numbers = re.findall(r'\d+', review)
-            inside_array.append(review.split('<br>')[0])
-            inside_array.append(numbers[0] + '%' + ' of positive reviews')
-            inside_array.append(numbers[1] + " total reviews")
-        else:
-            inside_array.append("Not Rated")
-            inside_array.append("Not Rated")
-            inside_array.append("Not Rated")
-
-        inside_array.append(row.find_element(By.CLASS_NAME, 'search_released').get_attribute('innerHTML'))
-        if len(row.find_element(By.CLASS_NAME, 'search_discount').get_attribute('innerHTML').strip()) > 0: 
-            inside_array.append('-' + row.find_element(By.CLASS_NAME, 'search_discount').get_attribute('innerHTML').split('%')[-2][-2:] + '%')
-        else:
-            inside_array.append("No Discount")
-        inside_array.append(row.find_element(By.CLASS_NAME, 'search_price').get_attribute('innerHTML').split('$')[-1].strip())
+            if len(row.find_elements(By.CLASS_NAME, 'win')) > 0:
+                inside_array.append(1)
+            else:
+                inside_array.append(0)
+            if len(row.find_elements(By.CLASS_NAME, 'mac')) > 0:
+                inside_array.append(1)
+            else:
+                inside_array.append(0)
+            if len(row.find_elements(By.CLASS_NAME, 'linux')) > 0:
+                inside_array.append(1)
+            else:
+                inside_array.append(0)
             
-    else:
-        inside_array = games_df.loc[games_df['Unique_ID'] == unique_id].values.flatten().tolist()
+            if len(row.find_elements(By.CLASS_NAME, 'positive')) > 0 or len(row.find_elements(By.CLASS_NAME, 'mixed')) > 0 or len(row.find_elements(By.CLASS_NAME, 'negative')) > 0:
+                review = row.find_element(By.CLASS_NAME, 'search_review_summary').get_attribute('data-tooltip-html')
+                numbers = re.findall(r'\d+', review)
+                inside_array.append(review.split('<br>')[0])
+                inside_array.append(numbers[0] + '%' + ' of positive reviews')
+                inside_array.append(numbers[1] + " total reviews")
+            else:
+                inside_array.append("Not Rated")
+                inside_array.append("Not Rated")
+                inside_array.append("Not Rated")
+
+            inside_array.append(row.find_element(By.CLASS_NAME, 'search_released').get_attribute('innerHTML'))
+            if len(row.find_element(By.CLASS_NAME, 'search_discount').get_attribute('innerHTML').strip()) > 0: 
+                inside_array.append('-' + row.find_element(By.CLASS_NAME, 'search_discount').get_attribute('innerHTML').split('%')[-2][-2:] + '%')
+            else:
+                inside_array.append("No Discount")
+                inside_array.append(row.find_element(By.CLASS_NAME, 'search_price').get_attribute('innerHTML').split('$')[-1].strip())
+                
+        else:
+            inside_array = games_df.loc[games_df['Unique_ID'] == unique_id].values.flatten().tolist()
         
-    outside_array.append(inside_array)
+        outside_array.append(inside_array)
 
     # uncomment to create csv file
     games = pd.DataFrame(outside_array, columns=['Unique_ID', 'url', 'title', 'tags', 'win_comp','mac_comp','linux_comp','review','percent_review',
                                                  'total_review', 'date_released', 'discount', 'price'])
 
-    games.to_sql(name='steam', con=engine, if_exists='replace')
+    games.to_sql(name='steam', con=engine, if_exists='replace', index=False)
 
     # return games
-    return (games.to_sql(name='steam', con=engine, if_exists='replace', index=False))
+    # return (games.to_sql(name='steam', con=engine, if_exists='replace', index=False))
 
 update_games()
 # df = update_games()
