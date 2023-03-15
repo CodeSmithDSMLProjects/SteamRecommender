b65776a6370ac8cce11345b1d885459bf6bdd4bb
Paul <pauljr97@gmail.com>
2023-03-15 10:06:09 -0700

    added github workflows and dockerized flask and streamlit

--- model/server.py
+++ model/server.py
@@ -1,42 +1,53 @@
 import json
 import pandas as pd
-from sqlalchemy import create_engine
+import psycopg2
+from sqlalchemy import create_engine, URL
 from flask import Flask, request, jsonify
-import pandas as pd
 import boto3
 import pickle as pkl
 import os
 from functions import load_file, topGames
 
-df = pd.read_csv('../data/steam.csv')
+# df = pd.read_csv('../data/steam.csv')
+# idDict = df.set_index('title').to_dict()['Unique_ID']
+# unpickled = pd.read_pickle("../data/cos_df.pkl")
+
+# S3 - Pickl
+session = boto3.Session(
+    aws_access_key_id = os.environ["ACCESS_KEY"],
+    aws_secret_access_key=os.environ["SECRET_KEY"],
+    # region_name = os.environ["REGION"]
+)
+
+s3client = session.client('s3')
+
+bucket = 'steamcos'
+file_name = 'cos_df.pkl'
+
+response = s3client.get_object(Bucket=bucket, Key=file_name)
+body = response['Body'].read()
+unpickled = pkl.loads(body)
+
+# SQL - RDS
+url_object = URL.create(
+                "postgresql+psycopg2",
+                host=os.environ["HOST"],
+                username=os.environ["USER"],
+                port=5432,
+                password=os.environ["PASSWORD"],
+                database=os.environ["DB"])
+
+engine = create_engine(url_object)
+engine.connect()
+connection = engine.raw_connection()
+
+sql = """
+SELECT * FROM steam
+"""
+
+df = pd.read_sql(sql, con=connection)
 idDict = df.set_index('title').to_dict()['Unique_ID']
-unpickled = pd.read_pickle("../data/cos_df.pkl")
 
-# session = boto3.Session(
-#     aws_access_key_id = os.environ["ACCESS_KEY"],
-#     aws_secret_access_key=os.environ["ACCESS_SECRET"],
-#     region_name = os.environ["REGION"]
-# )
-
-# url_object = URL.create(
-#                 "postgresql+psycopg2",
-#                 host=os.environ["HOST"],
-#                 username=os.environ["USER"],
-#                 port=5432,
-#                 password=os.environ["PASSWORD"],
-#                 database=os.environ["DB"])
-
-# engine = create_engine(url_object)
-# engine.connect()
-# connection = engine.raw_connection()
-
-# s3 = session.resource('s3')
-# URI = os.environ["URI"]
-# engine = create_engine(URI)
-
-# similarity_df = pd.read_sql_table('models_score', con=engine)
-
-# mod_api = pkl.loads(s3.Bucket("carsalesmodel").Object(f'{name}.pkl').get()['Body'].read())
 
 app = Flask(__name__)
 
@@ -57,7 +68,8 @@ def predict_streamlit():
 def get_games():
 
     data = df['title']
-    res = data.to_json()
+
+    res = data.to_json(orient= 'values')
     load = json.loads(res)
     return json.dumps(load)
 
@@ -67,7 +79,6 @@ def predict():
 
     data = request.get_data()
     data = data.decode("utf-8")
-    # data = json.loads(data)
     pred = topGames(idDict[data], unpickled, 10)
     res = pred.to_json()
     load = json.loads(res)
--- model/streamlit.py
+++ model/streamlit.py
@@ -1,40 +1,38 @@
 import streamlit as st
-import boto3
-import pickle
 import pandas as pd
 from streamlit_option_menu import option_menu
-import os
 from sqlalchemy import create_engine, text, URL
 import json
 import requests as rq
+import os
 
 st.set_page_config(layout="wide", page_title="Steam Games Recommender")
 
-df = pd.read_csv('../data/steam.csv')
+# df = pd.read_csv('../data/steam.csv')
 
 
-# url_object = URL.create(
-#                 "postgresql+psycopg2",
-#                 host=os.environ["HOST"],
-#                 username=os.environ["USER"],
-#                 port=5432,
-#                 password=os.environ["PASSWORD"],
-#                 database=os.environ["DB"])
+# SQL - RDS
+url_object = URL.create(
+                "postgresql+psycopg2",
+                host=os.environ["HOST"],
+                username=os.environ["USER"],
+                port=5432,
+                password=os.environ["PASSWORD"],
+                database=os.environ["DB"])
 
-# session = boto3.Session(
-#     aws_access_key_id = os.environ["ACCESS_KEY"],
-#     aws_secret_access_key=os.environ["ACCESS_SECRET"],
-#     region_name = os.environ["REGION"]
-# )
+engine = create_engine(url_object)
+engine.connect()
+connection = engine.raw_connection()
 
-# engine = create_engine(url_object)
+sql = """
+SELECT * FROM steam
+"""
 
-# GAMES_SQL_QUERY = 'SELECT DISTINCT "title" FROM steam'
-
-# with engine.connect() as connection:
-#     games_df = pd.read_sql_query(GAMES_SQL_QUERY, con=connection)
+df = pd.read_sql(sql, con=connection)
 
 selected_navbar = option_menu(None, ["Predict", "FAQ", "API"], orientation="horizontal")
+
+url     =  'http://0.0.0.0:8080/predict_streamlit'
 
 if selected_navbar == "Predict":
     with st.container():
@@ -50,8 +48,8 @@ if selected_navbar == "Predict":
         button = st.form_submit_button(label="Submit", use_container_width=True)
 
     if button:
+        # Test connection to server
         # Connect to Flask Predict
-        url     =  'http://127.0.0.1:8080/predict_streamlit'
         payload = json.dumps(option)
         resp    = rq.post(url, json = payload)
         try:    
